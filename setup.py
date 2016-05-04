32fde79c5a567823bf1e819de86915bab5660352
Johan Nestaas <johannestaas@gmail.com>
2016-05-04 00:12:16 -0700

    setup

--- setup.py
+++ setup.py
@@ -1,33 +1,36 @@
 import os
 from setuptools import setup
 
-# cursed
-# Simplified curses
+#  cursed
+#  Simplified curses
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "cursed",
-    version = "0.0.1",
-    description = "Simplified curses",
-    author = "Johan Nestaas",
-    author_email = "johannestaas@gmail.com",
-    license = "GPLv3+",
-    keywords = "",
-    url = "https://www.bitbucket.org/johannestaas/cursed",
+    name="cursed",
+    version="0.0.1",
+    description="Simplified curses",
+    author="Johan Nestaas",
+    author_email="johannestaas@gmail.com",
+    license="GPLv3+",
+    keywords="",
+    url="https://www.bitbucket.org/johannestaas/cursed",
     packages=['cursed'],
     package_dir={'cursed': 'cursed'},
     long_description=read('README.rst'),
     classifiers=[
-        #'Development Status :: 1 - Planning',
-        #'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
         'Development Status :: 3 - Alpha',
-        #'Development Status :: 4 - Beta',
-        #'Development Status :: 5 - Production/Stable',
-        #'Development Status :: 6 - Mature',
-        #'Development Status :: 7 - Inactive',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'License :: OSI Approved :: GNU General Public License v3 or later '
+        '(GPLv3+)',
         'Environment :: Console',
         'Environment :: X11 Applications :: Qt',
         'Environment :: MacOS X',
@@ -39,13 +42,13 @@ setup(
     ],
     install_requires=[
     ],
-    entry_points = {
-        'console_scripts': [
-            'cursed = cursed:main',
-        ],
+    entry_points={
+        # 'console_scripts': [
+        #     'cursed=cursed:main',
+        # ],
     },
-    #package_data = {
-        #'cursed': ['catalog/*.edb'],
-    #},
-    #include_package_data = True,
+    # package_data={
+    # 'cursed': ['catalog/*.edb'],
+    # },
+    # include_package_data=True,
 )
