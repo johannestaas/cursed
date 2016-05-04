e52026fab0948377d1ab054a1740aca746d9f064
Johan Nestaas <johannestaas@gmail.com>
2016-05-04 01:05:16 -0700

    return Result

--- cursed/app.py
+++ cursed/app.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
+import sys
 import traceback
 import curses
 
 
 class CursedWindow(object):
 
-    def __init__(self, cls, width=80, height=24, x=0, y=0):
+    def __init__(self, app, cls, width=80, height=24, x=0, y=0):
+        self.app = app
         self.cls = cls
         self.window = None
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        self.fix_class(cls)
+        self.fix_class()
 
     def run(self, window):
         self.window = window.subwin(self.height, self.width, self.y, self.x)
@@ -21,11 +23,42 @@ class CursedWindow(object):
         return self.window.addch(y, x, c)
 
     def getch(self):
-        return self.window.getch()
+        return self.app.getch()
+
+    def refresh(self):
+        self.window.refresh()
 
     def fix_class(self):
-        self.cls.addch = self.addch
-        self.cls.getch = self.getch
+        for attr in ('addch', 'getch', 'refresh'):
+            setattr(self.cls, attr, getattr(self, attr))
+
+
+class Result(object):
+
+    def __init__(self):
+        self.interrupt = False
+        self.tb = None
+        self.exception = None
+
+    def unwrap(self, reraise=True):
+        self.print_tb()
+        if self.exception and reraise:
+            raise self.exception
+
+    def ok(self):
+        return not any(self.tb, self.exception, self.interrupt)
+
+    def err(self):
+        if self.ok():
+            return None
+        if self.exception:
+            return self.exception
+        if self.interrupt:
+            return KeyboardInterrupt
+
+    def print_tb(self):
+        if self.tb:
+            sys.stderr.write(self.tb)
 
 
 class CursedApp(object):
@@ -37,18 +70,22 @@ class CursedApp(object):
 
     def window(self, *args, **kwargs):
         def decorator(cls):
-            self.windows[cls] = CursedWindow(cls, **kwargs)
+            self.windows[cls] = CursedWindow(self, cls, **kwargs)
             return cls
         if len(args) == 1:
             return decorator(args[0])
         else:
             return decorator
 
+    def getch(self):
+        return self.scr.getch()
+
     def run_windows(self):
         for cls, cw in self.windows.items():
             cw.run(self.window)
 
     def run(self, loop_func, *args, **kwargs):
+        result = Result()
         try:
             self.scr = curses.initscr()
             curses.noecho()
@@ -57,11 +94,15 @@ class CursedApp(object):
             self.window.keypad(1)
             self.run_windows()
             loop_func(*args, **kwargs)
-        except Exception:
-            traceback.print_exc()
+        except KeyboardInterrupt:
+            result.interrupt = True
+        except Exception as e:
+            result.tb = traceback.format_exc()
+            result.exception = e
         finally:
             if self.scr is not None:
                 self.scr.keypad(0)
             curses.echo()
             curses.nocbreak()
             curses.endwin()
+        return result
