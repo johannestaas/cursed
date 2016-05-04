5c0e60364f1f8f154999a13d9b4e51fadba4b29b
Johan Nestaas <johannestaas@gmail.com>
2016-05-04 00:09:23 -0700

    app start

--- cursed/app.py
+++ cursed/app.py
@@ -7,23 +7,21 @@ class CursedApp(object):
 
     def __init__(self):
         self.scr = None
+        self.menu = None
+        self.windows = []
 
-    def __enter__(self):
-        self.scr = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
-        self.window = self.scr.subwin(0, 0)
-        self.window.keypad(1)
-        self.menu_panel = curses.panel.new_panel(self.window)
-        self.panel.hide()
-        curses.panel.update_panels()
-        return self
-
-    def __exit__(self, *excs):
-        if self.scr is None:
-            return
-        self.scr.keypad(0)
-        curses.echo()
-        curses.nocbreak()
-        curses.endwin()
-        traceback.print_exc()
+    def run(self):
+        try:
+            self.scr = curses.initscr()
+            curses.noecho()
+            curses.cbreak()
+            self.window = self.scr.subwin(0, 0)
+            self.window.keypad(1)
+        except Exception:
+            traceback.print_exc()
+        finally:
+            if self.scr is not None:
+                self.scr.keypad(0)
+            curses.echo()
+            curses.nocbreak()
+            curses.endwin()
