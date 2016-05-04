f3047b2c088f318597ffff372730c71fbc9a5991
Johan Nestaas <johannestaas@gmail.com>
2016-05-04 00:31:19 -0700

    basic decoration

--- cursed/app.py
+++ cursed/app.py
@@ -3,20 +3,60 @@ import traceback
 import curses
 
 
+class CursedWindow(object):
+
+    def __init__(self, cls, width=80, height=24, x=0, y=0):
+        self.cls = cls
+        self.window = None
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.fix_class(cls)
+
+    def run(self, window):
+        self.window = window.subwin(self.height, self.width, self.y, self.x)
+
+    def addch(self, x, y, c):
+        return self.window.addch(y, x, c)
+
+    def getch(self):
+        return self.window.getch()
+
+    def fix_class(self):
+        self.cls.addch = self.addch
+        self.cls.getch = self.getch
+
+
 class CursedApp(object):
 
     def __init__(self):
         self.scr = None
         self.menu = None
-        self.windows = []
+        self.windows = {}
 
-    def run(self):
+    def window(self, *args, **kwargs):
+        def decorator(cls):
+            self.windows[cls] = CursedWindow(cls, **kwargs)
+            return cls
+        if len(args) == 1:
+            return decorator(args[0])
+        else:
+            return decorator
+
+    def run_windows(self):
+        for cls, cw in self.windows.items():
+            cw.run(self.window)
+
+    def run(self, loop_func, *args, **kwargs):
         try:
             self.scr = curses.initscr()
             curses.noecho()
             curses.cbreak()
             self.window = self.scr.subwin(0, 0)
             self.window.keypad(1)
+            self.run_windows()
+            loop_func(*args, **kwargs)
         except Exception:
             traceback.print_exc()
         finally:
