19eb26ce510e993ec3f018b59739636c339368d0
Johan Nestaas <johannestaas@gmail.com>
2016-05-26 20:18:10 -0800

    Fix browser to display in two windows

--- examples/browser.py
+++ examples/browser.py
@@ -5,7 +5,7 @@ app = CursedApp()
 
 
 class HeaderWindow(CursedWindow):
-    HEIGHT = 24
+    HEIGHT = 3
 
     @classmethod
     def update(cls):
@@ -13,25 +13,20 @@ class HeaderWindow(CursedWindow):
         cls.hline(0, 1)
         url = cls.getstr(0, 2, prompt='URL: ')
         if url:
-            HeaderWindow.trigger('get_request', url)
+            DisplayWindow.trigger('get_request', url)
+
+
+class DisplayWindow(CursedWindow):
+    Y = 3
+    HEIGHT = 21
+    BORDERED = True
 
     @classmethod
     def get_request(cls, url):
         cls.response = requests.get(url)
         cls.redraw()
-        cls.write(cls.response.content, 0, 3)
+        cls.write(cls.response.content, 0, 0)
 
-'''
-# Fix - seems not to getstr with other window
-class DisplayWindow(CursedWindow):
-    HEIGHT = 21
-
-    @classmethod
-    def get_request(cls, url):
-        cls.response = requests.get(url)
-        # cls.redraw()
-        cls.write(cls.response.content)
-'''
 
 result = app.run()
 print(result)
