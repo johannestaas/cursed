d5bf2e462f21c6f7556a761fbced51b80817bbb9
Johan Nestaas <johannestaas@gmail.com>
2018-07-21 01:05:23 -0700

    0.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="cursed",
-    version="0.2.0",
+    version="0.2.1",
     description="Simplified curses",
     author="Johan Nestaas",
     author_email="johannestaas@gmail.com",
